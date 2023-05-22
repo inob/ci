d683929dd5e2d638efe689d1a69f7cd269d6e532
inob <romansim03@mail.ru>
2023-05-22 20:08:33 +0300

    upd

--- main.py
+++ main.py
@@ -1,17 +1,143 @@
 import sys
 from PyQt5 import QtWidgets
-import Login
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
+import requests
+from bs4 import BeautifulSoup
+from util import read_text, primfacs, is_prime
+from PyQt5.QtWidgets import * 
+from PyQt5.QtGui import *
+from PyQt5 import QtCore
+from math import gcd
 
-class ExampleApp(QtWidgets.QMainWindow, Login.Ui_MainWindow):
+class Login(QDialog):
     def __init__(self):
-        super().__init__()
-        self.setupUi(self) #Инициализируем дизайн
+        super(Login,self).__init__()
+        loadUi("Login.ui",self)
+        self.auth.clicked.connect(self.loginF)
+        self.pushButton_3.clicked.connect(self.gotocreate)
+    
+    def loginF(self):
+        if self.login.text() and self.password.text():
+            login = self.login.text()
+            password = self.password.text()
+            page = requests.post('http://127.0.0.1/Mem/safe.php', data={'login':login, 'password':password})
+            soup = BeautifulSoup(page.content, "html5lib")
+            print(soup.text)
+            self.label.setText(soup.text)
+            if "good" in soup.text:
+                self.gotoChoose()
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    window = ExampleApp()
-    window.shop()
-    app.exec_()
+    def gotocreate(self):
+        createacc = Registr()
+        widget.addWidget(createacc)
+        widget.setCurrentIndex(widget.currentIndex()+1)
 
-if __name__ == '__main__':
-    main()
+    def gotoChoose(self):
+        choosee = Choose()
+        widget.addWidget(choosee)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class Choose(QDialog):
+    def __init__(self):
+        super(Choose,self).__init__()
+        loadUi("Choose.ui",self)
+        self.buttonFerma.clicked.connect(self.goFerma)
+
+    def goFerma(self):
+        ferma = Ferma()
+        widget.addWidget(ferma)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class Ferma(QDialog):
+    def __init__(self):
+        super(Ferma,self).__init__()
+        loadUi("Ferma.ui",self)
+        widget.setFixedWidth(707)
+        widget.setFixedHeight(735)
+        text = read_text('Ferm_Theory.html')
+        label_text = QLabel(text)
+        label_text.setWordWrap(True)
+        self.scrollArea.setWidget(label_text)
+        self.pushButton.clicked.connect(self.answer)
+
+    def answer(self):
+        n = int(self.lineEdit.text())
+        a = int(self.lineEdit_2.text())
+        if gcd(n,a)==1 and n>=2 and a >= 2:
+            n-=1
+            x = [int(x) for x in primfacs(n)]
+            num = "("*len(x) + f"{a}"
+            for i in x:
+                num+=f")<sup>{i}</sup>"
+            print(x)
+            if is_prime(n+1):    
+                self.label_5.setText(f"""<p> {a}<sup>{n}</sup> &equiv; {num} &equiv; 1(mod {n+1})</p>
+                                         <p> Число {n+1}, вероятнее всего, <b>простое.</b>""")
+
+                self.label_5.setWordWrap(True)
+            else:
+                self.label_5.setText(f"""<p> {a}<sup>{n}</sup> &equiv; {num} &ne; 1(mod {n+1})</p>
+                                         <p> Число {n+1} - <b>составное.</b>""")
+                self.label_5.setWordWrap(True)
+        else:
+            label_5 = QLabel("Числа не взаимно простые.")
+
+
+class SV(QDialog):
+    def __init__(self):
+        super(Ferma,self).__init__()
+        loadUi("Ferma.ui",self)
+        widget.setFixedWidth(707)
+        widget.setFixedHeight(735)
+        text = read_text('MR_Theory.html')
+        label_text = QLabel(text)
+        label_text.setWordWrap(True)
+        self.scrollArea.setWidget(label_text)
+
+class Miller(QDialog):
+    def __init__(self):
+        super(Ferma,self).__init__()
+        loadUi("Ferma.ui",self)
+        widget.setFixedWidth(707)
+        widget.setFixedHeight(735)
+        text = read_text('SV_Theory.html')
+        label_text = QLabel(text)
+        label_text.setWordWrap(True)
+        self.scrollArea.setWidget(label_text)
+
+class Registr(QDialog):
+    def __init__(self):
+        super(Registr,self).__init__()
+        loadUi("Registration.ui",self)
+        self.signupbutton.clicked.connect(self.createAcc)
+        self.auth_back.clicked.connect(self.authBack)
+
+    def authBack(self):
+        login = Login()
+        widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def createAcc(self):
+        if self.login.text() and self.password.text():
+            login = self.login.text()
+            password = self.password.text()
+            cpassword = self.confirmpass.text()
+            email = self.email.text()
+            page = requests.post('http://127.0.0.1/Mem/registration.php', data={'login':login, 'password':password, 'email':email})
+            soup = BeautifulSoup(page.content, "html5lib")
+            print(soup.text)
+            if soup.text: self.authBack() 
+            else: self.label_5.setText("Регистрация не прошла !")
+
+
+
+
+app=QApplication(sys.argv)
+mainwindow=Login()
+widget=QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedWidth(960)
+widget.setFixedHeight(620)
+widget.show()
+app.exec_()  
---
+++ util.py
@@ -0,0 +1,160 @@
+# This Python file uses the following encoding: windows-1251
+
+from typing import Tuple, List
+from random import randint, choice
+from math import gcd, sqrt
+
+
+def read_text(FILENAME):
+    f = open(FILENAME, 'r', encoding='utf8')
+    ot = f.read()
+    f.close()
+    return ot
+
+def read_text_list(filename):
+    text = []
+    with open(filename, 'r') as f:
+        text.append(f.readline())
+    return text
+
+def get_random_message(size: int) -> str:
+    # ��������� ��������� ������
+    m = []
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    for i in range(size):
+        m.append(choice(alpha))
+    res = ""
+    res = res.join(m)
+    return res
+    
+def get_prime_numbers_in_range(start: int, finish: int) -> List[int]:
+    # ������� ��� ������� ����� �� �������� ����������
+    primes = [] # ������ ������� ����� �� ���������
+    # ������ � ������� True - ����� �������, False - ����� ���������
+    is_prime = [True] * (finish + 1)
+    # 0 � 1 �� �������
+    is_prime[0] = is_prime[1] = False
+    # ������ ���������� i - �����, is_prime_i - ������� ��� ���
+    for i, is_prime_i in enumerate(is_prime):
+        if is_prime_i:
+            # ��������� ������� ����� � ������, ���� ��� � ���������
+            if i >= start:
+                primes.append(i)
+            # ��� ����� �� i * i �� finish � ����� i ������ i
+            for j in range(i * i, finish + 1, i):
+                is_prime[j] = False
+    return primes
+
+def get_prime_number_in_range(start: int, finish: int) -> int:
+    # ������� ������� ����o �� ���������� [start, finish]
+    res = get_prime_numbers_in_range(start, finish)
+    return res[randint(2, len(res) - 1)]
+
+def extended_gcd(a: int, b: int) -> Tuple[int, int, int]:
+    # ������������ �������� �������
+    a0 = a
+    b0 = b
+    t0 = 0
+    t = 1
+    s0 = 1
+    s = 0
+    q = a0 // b0
+    r = a0 - q * b0
+    while r > 0:
+        temp = t0 - q * t
+        t0 = t
+        t = temp
+        temp = s0 - q * s
+        s0 = s
+        s = temp
+        a0 = b0
+        b0 = r
+        q = a0 // b0
+        r = a0 - q * b0
+    r = b0
+    return r, s, t
+
+def binary_pow(a: int, b: int, p: int) -> int:
+    # �������� ��������� ���������� � �������
+    # ���� ������� ����� 0, �� ��������� ����� 1
+    if b == 0:
+        return 1
+    # ������������� d � �������� �������
+    # ������� bin() ���������� ������ ���� '0b1010'
+    # ������� ������������� � �������� ������� ���������� �� 2 �������
+    d = bin(b)[2:]
+    r = len(d) - 1
+    # �������� ������ a_i
+    a_ = [0] * (r + 1)
+    # ������� a_0 = a
+    a_[0] = a
+    # ��������� a_i ��� i = 1,...,r, r = len(d) - 1
+    for i in range(1, r + 1):
+        # ��������� a_i
+        a_[i] = (a_[i - 1] ** 2) * (a ** int(d[i]))
+        # ����� ������� �� ������
+        a_[i] %= p
+    # ���������� a_r �� ������ ��� ������� ����� d = 1
+    return a_[r] % p
+
+def modular_inv(number: int, mod: int) -> int:
+    # ������� �������� ����� �� ������ ��� �����
+    # ������� ��� ������ � ����� � ������������ ����������� ����
+    # ��������� ����������� �������� �������.
+    t, x, y = extended_gcd(number, mod)
+    # ���� ��� �� ����� 1, �� ��������� ����� ���
+    if t != 1:
+        raise ValueError('----')
+    # ��� ������������� ����� ����� ��������� ������
+    return (x % mod + mod) % mod
+
+def get_coprime_in_range(
+        start: int, finish: int, number: int) -> int:
+    # ������� ����� �� ���������� [start, finish] ������� ������� � number
+    for i in range(start, finish):
+        if gcd(number, i) == 1:
+            return i
+    return i
+
+def is_prime(n: int) -> bool:
+    # ���������, �������� �� ����� �������.
+    if n == 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def check_values(a: int, b: int, p: int) -> bool:
+    # ��������� �������� ���������� ���������
+    #
+    return gcd(a, p) == 1 and gcd(b, p) == 1 and p != 2 and is_prime(p)
+
+def get_values(number_of_runs: int) -> List[Tuple[int, int, int]]:
+    # ������� ���������� �������� ��� �������� ������� ����������.
+    # �������� ����������� ��� ������������ �������� �������.
+    values = []
+    for _ in range(number_of_runs):
+        a, b, p = 0, 0, 0
+        # ���� ����� �� ������������� �������� ������
+        # ���������� ����� ��������
+        while not check_values(a, b, p):
+            a, b, p = randint(1, 900), randint(1, 900), randint(1, 900)
+        values.append((a, b, p))
+    return values
+
+def primfacs(n):
+   i = 2
+   primfac = []
+   while i * i <= n:
+       while n % i == 0:
+           primfac.append(i)
+           n = n / i
+       i = i + 1
+   if n > 1:
+       primfac.append(n)
+   return primfac
